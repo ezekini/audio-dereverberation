8b1366ba6d5b9950fa6fdf4d91c607e3a5f02517
elece <ezekini@users.noreply.github.com>
2020-01-12 21:17:34 -0300

    Update convolution.py

--- convolution.py
+++ convolution.py
@@ -3,10 +3,10 @@ import numpy as np
 from scipy import signal
 import soundfile as sf
 
-#Directories of .wav files ending with '/'
+# Directories of .wav files ending with '/'
 anechoic_dir = '/Volumes/Material/Eze/Drive/Tesis-Ezequiel/00-Audios/anechoic/'
 rir_dir = '/Volumes/Material/Eze/Drive/Tesis-Ezequiel/00-Audios/rir/'
-#Search for .wav files in the given directories and build the filenames list
+# Search for .wav files in the given directories and build the filenames list
 anechoic_filenames = [_ for _ in os.listdir(anechoic_dir) if _.endswith(".wav")]
 rir_filenames = [_ for _ in os.listdir(rir_dir) if _.endswith(".wav")]
 
@@ -16,18 +16,18 @@ sr = int(16e3)
 
 for i in range(len(anechoic_filenames)):
     anechoic_fn = anechoic_filenames[i]
-    #reads wav file and converts to float64
-    anechoic, _ =  sf.read(anechoic_dir + anechoic_fn)
+    # reads wav file and converts to float64
+    anechoic, _ = sf.read(anechoic_dir + anechoic_fn)
 #    anechoicL = len(anechoic)
 
     for j in range(len(rir_filenames)):
         rir_fn = rir_filenames[j]
-        #reads wav file and converts to float64
-        rir, _ =  sf.read(rir_dir + rir_fn)
+        # reads wav file and converts to float64
+        rir, _ = sf.read(rir_dir + rir_fn)
         #rirL = len(rir)
-        #zero pad to have length rir+anechoic-1
+        # zero pad to have length rir+anechoic-1
         #rir = np.append(rir, anechoicL-1)
-        #zero pad to have length rir+anechoic-1
+        # zero pad to have length rir+anechoic-1
         #anechoic = np.append(anechoic, rirL-1)
         conv = signal.convolve(anechoic, rir, method='auto')
 
